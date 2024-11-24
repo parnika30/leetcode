afe59cb732e3d1b040f828c804b634f468145b8b
Parnika Roy <79538861+parnika30@users.noreply.github.com>
2024-11-24 16:26:51 +0530

    Time: 65 ms (23.87%), Space: 51.5 MB (66.32%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,29 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+
+var createCounter = function(init) {
+    let count = init;
+    return {
+        increment : () => (count+=1),
+        reset : () => (count=init), 
+        decrement : () => (count-=1)
+    }
+};
+
+// var createCounter = function(init) {
+//     let count=init;
+//     return {
+//         increment:()=> (count+=1),
+//         decrement:()=> (count-=1),
+//         reset:()=> (count=init)
+//     }
+
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
