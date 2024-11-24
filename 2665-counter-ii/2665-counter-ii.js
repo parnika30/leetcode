2dd3673222c2bfe7ecead221cf1d8975aca53836
Parnika Roy <79538861+parnika30@users.noreply.github.com>
2024-11-24 16:26:56 +0530

    Time: 52 ms (88.26%), Space: 51.4 MB (74.30%) - LeetHub

--- 2665-counter-ii/2665-counter-ii.js
+++ 2665-counter-ii/2665-counter-ii.js
@@ -12,14 +12,6 @@ var createCounter = function(init) {
     }
 };
 
-// var createCounter = function(init) {
-//     let count=init;
-//     return {
-//         increment:()=> (count+=1),
-//         decrement:()=> (count-=1),
-//         reset:()=> (count=init)
-//     }
-
 
 /**
  * const counter = createCounter(5)
