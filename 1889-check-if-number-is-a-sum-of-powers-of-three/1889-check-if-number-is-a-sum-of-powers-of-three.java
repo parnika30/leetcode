61a3c750962b8a273f96e8af811fd6f9df152728
Parnika Roy <79538861+parnika30@users.noreply.github.com>
2025-03-05 00:43:44 +0530

    Time: 0 ms (100%), Space: 40.7 MB (32.85%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.java
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.java
@@ -1,10 +1,25 @@
+// class Solution {
+//     public boolean checkPowersOfThree(int n) {
+//         String base3 = Integer.toString(n, 3);
+//         if(base3.contains("2")){
+//             return false;
+//         }else{
+//             return true;
+//         }
+//     }
+// }
+
+
 class Solution {
     public boolean checkPowersOfThree(int n) {
-        String base3 = Integer.toString(n, 3);
-        if(base3.contains("2")){
-            return false;
-        }else{
-            return true;
+        while(n>0){
+            //construct the ternary representation of the given number by taking its remainder when divided by 3 (mod 3)
+            if(n%3 == 2) return false; 
+            n/=3;
+            //dividing it by 3 to proceed to the next digit
+            //if any of these remainders equals 2, 
+            //we would need to use a power of 3 twice, which is not allowed. In that case, we immediately return false.
         }
+        return true;
     }
 }
