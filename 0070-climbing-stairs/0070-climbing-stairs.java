0480fb5dc34ce7d787b7803b15f1d39867adee61
Parnika Roy <79538861+parnika30@users.noreply.github.com>
2025-03-07 00:23:31 +0530

    Time: 0 ms (100%), Space: 40.8 MB (17.91%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,11 @@
+class Solution {
+    HashMap <Integer,Integer> hs = new HashMap<>();
+    public int climbStairs(int n) {
+        if(n == 0 || n == 1) return 1;
+        if (hs.containsKey(n)) return hs.get(n);
+        int res = climbStairs(n-1) + climbStairs(n-2); 
+        hs.put(n,res);
+        return res; 
+    }
+
+}
