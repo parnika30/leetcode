fb166571321f5cac60cd08b432e1449560f6740e
Parnika Roy <79538861+parnika30@users.noreply.github.com>
2025-03-12 02:24:51 +0530

    Time: 11 ms (91.15%), Space: 44.6 MB (79.4%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
@@ -42,13 +42,16 @@ class Solution {
     public int numberOfSubstrings(String s) {
         int n = s.length();
         int count = 0;
-        int[] last = {-1, -1, -1};
+        int[] last = {-1, -1, -1}; 
+        //last[0] stores the last index of 'a', last[1] for 'b', and last[2] for 'c'.
 
 
         for (int i = 0; i < n; i++) {
-            last[s.charAt(i)-'a'] = i;
+            last[s.charAt(i)-'a'] = i; //For every character s[i], update its last seen index.
             int min1 = Math.min(last[0],last[1]);
             count = count+(1 + Math.min(min1,last[2]));
+
+            // Once we have seen 'a', 'b', and 'c', the minimum index among them tells us the starting index of the shortest valid substring ending at i.
         }
         return count;
     }
