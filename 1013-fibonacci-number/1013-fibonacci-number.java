d7663fccbfbafab13b00975fcff8b5739973d13b
Parnika Roy <79538861+parnika30@users.noreply.github.com>
2025-04-12 13:14:04 +0530

    Time: 18 ms (11.74%), Space: 41 MB (6.52%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,6 +1,22 @@
+//recursion 
+// class Solution {
+//     public int fib(int n) {
+//         if(n<=1) {return n;}
+//         else{return (fib(n-1) + fib(n-2));}
+//     }
+// }
+
+//dp
 class Solution {
+    private int dpFib(int[] dp, int n){
+        if(n<=1) return n;
+        if(dp[n] != -1) return dp[n]; 
+        //if not -1, dp array will have some value, that means, value is stored for that recursion. 
+        return (dpFib(dp, n-1) + dpFib(dp, n-2));
+    }
     public int fib(int n) {
-        if(n<=1) {return n;}
-        else{return (fib(n-1) + fib(n-2));}
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return dpFib(dp, n);
     }
 }
