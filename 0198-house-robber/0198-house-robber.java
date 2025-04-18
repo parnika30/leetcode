f948a1f88e6ce039097982c146ede4128d9d6380
Parnika Roy <79538861+parnika30@users.noreply.github.com>
2025-04-18 16:32:11 +0530

    Time: 0 ms (100%), Space: 41 MB (60.78%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,20 +1,36 @@
+//tle -> 2^n
+// class Solution {
+//     public int rob(int[] nums) {
+//         int n = nums.length;
+//         return recursion(n-1, nums);
+//     }
+
+//     private int recursion(int index, int[] nums){
+//         if(index < 0) return 0 ;
+//         if(index == 0) return nums[index];
+
+//         int pick = nums[index] + recursion(index-2, nums);
+//         int notPick = 0 + recursion(index-1, nums);
+//         return Math.max(pick, notPick);
+//     }
+// }
+
+//memoization
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-
-        if (n == 1) {
-            return nums[0];
-        }
-
         int[] dp = new int[n];
-
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-
-        for (int i = 2; i < n; i++) {
-            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
-        }
-
-        return dp[n - 1];        
+        Arrays.fill(dp,-1);
+        return recursion(n-1, nums, dp);
     }
-} // done for the sake of streak. revise again. 
+
+    private int recursion(int index, int[] nums, int[] dp){
+        if(index < 0) return 0 ;
+        if(index == 0) return nums[index];
+        if(dp[index]!=-1) return dp[index];
+
+        int pick = nums[index] + recursion(index-2, nums, dp);
+        int notPick = 0 + recursion(index-1, nums,dp);
+        return dp[index] = Math.max(pick, notPick); //initialized the value
+    }
+}
