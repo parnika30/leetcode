497fc384c920bff4b2d0dafa2b78ee1ca9c72811
Parnika Roy <79538861+parnika30@users.noreply.github.com>
2025-04-18 20:20:45 +0530

    Time: 0 ms (100%), Space: 41.5 MB (15.31%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -15,22 +15,42 @@
 //     }
 // }
 
-//memoization
+//memoization - tc = O(n)
+// sc - O(n)(extra for dp array) +  O(N)
+// class Solution 
+//     public int rob(int[] nums) {
+//         int n = nums.length;
+//         int[] dp = new int[n];
+//         Arrays.fill(dp,-1);
+//         return recursion(n-1, nums, dp);
+//     }
+
+//     private int recursion(int index, int[] nums, int[] dp){
+//         if(index < 0) return 0 ;
+//         if(index == 0) return nums[index];
+//         if(dp[index]!=-1) return dp[index];
+
+//         int pick = nums[index] + recursion(index-2, nums, dp);
+//         int notPick = 0 + recursion(index-1, nums,dp);
+//         return dp[index] = Math.max(pick, notPick); //initialized the value
+//     }
+// }
+
+
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp,-1);
-        return recursion(n-1, nums, dp);
-    }
+        int prev = nums[0];
+        int prev2 = 0;
+        for (int i =1 ;i<n;i++){
+            int take = nums[i];
+            if(i>1) take += prev2;
+            int leave = 0+prev;
 
-    private int recursion(int index, int[] nums, int[] dp){
-        if(index < 0) return 0 ;
-        if(index == 0) return nums[index];
-        if(dp[index]!=-1) return dp[index];
-
-        int pick = nums[index] + recursion(index-2, nums, dp);
-        int notPick = 0 + recursion(index-1, nums,dp);
-        return dp[index] = Math.max(pick, notPick); //initialized the value
+            int curr = Math.max(take,leave);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
     }
 }
