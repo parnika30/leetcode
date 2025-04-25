eb711d6dde2e11bc00c2e54f9e83142da6b71d8b
Parnika Roy <79538861+parnika30@users.noreply.github.com>
2025-04-25 22:41:21 +0530

    Time: 7 ms (49.38%), Space: 46.1 MB (13.4%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+    
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int[] edge : prerequisites) {
+            int u = edge[0];
+            int v = edge[1];
+            adj.get(u).add(v);
+        }
+        
+        int[] indeg = new int[numCourses];
+        //figuring out the indegree of each element 
+        for(int i =0; i< numCourses ;i++){
+            for(int ele : adj.get(i)){
+                indeg[ele]++; //indeg[1]++ (incoming edge to 1)
+            }
+        }
+        
+        Queue<Integer> q = new LinkedList<Integer>();
+        for(int i =0; i< numCourses ;i++){
+            if(indeg[i]==0) q.add(i); //start from the ele whose indeg is 0
+        }
+            
+        ArrayList<Integer> topo = new ArrayList<>();
+        int i = 0;
+        while(!q.isEmpty()){
+            int node = q.peek(); //first ele whose indeg is 0
+            q.remove();
+            topo.add(node);
+            
+            //since node is in topo sort, remove from indegree
+            for(int ele : adj.get(node)){
+                indeg[ele]--;
+                if(indeg[ele] == 0) q.add(ele);
+            }
+        }
+        
+        //topo sort will always have same number of elements, 
+        //so if the number of ele are less than or greater than numCourses,
+        //it has a cycle, i.e. it can be arranged in more than 1 ways
+        if(topo.size()<numCourses ) return false;
+        else return true;
+    }
+}
