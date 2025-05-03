953408e4e43596f955aef03a20cc01b64a46bbfa
Parnika Roy <79538861+parnika30@users.noreply.github.com>
2025-05-03 19:28:19 +0530

    Time: 3 ms (99.25%), Space: 45.8 MB (80.7%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,4 +1,65 @@
+// class Solution {
+//     public int[] findOrder(int numCourses, int[][] prerequisites) {
+//        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+//         for (int i = 0; i < numCourses; i++) {
+//             adj.add(new ArrayList<>());
+//         }
+
+//         for (int[] edge : prerequisites) {
+//             int u = edge[1]; // course
+//             int v = edge[0]; // preprerequisite
+//             adj.get(u).add(v);
+//         }
+        
+//         int[] indeg = new int[numCourses];
+//         //figuring out the indegree of each element 
+//         for(int i =0; i< numCourses ;i++){
+//             for(int ele : adj.get(i)){
+//                 indeg[ele]++; //indeg[1]++ (incoming edge to 1)
+//             }
+//         }
+        
+//         Queue<Integer> q = new LinkedList<Integer>();
+//         for(int i =0; i< numCourses ;i++){
+//             if(indeg[i]==0) q.add(i); //start from the ele whose indeg is 0
+//         }
+            
+//         //ArrayList<Integer> topo = new ArrayList<>();
+//         int[] topo = new int[numCourses];
+//         int i = 0;
+//         while(!q.isEmpty()){
+//             int node = q.peek(); 
+//             q.remove();
+//             topo[i++] = node;
+            
+//             //since node is in topo sort, remove from indegree
+//             for(int ele : adj.get(node)){
+//                 indeg[ele]--;
+//                 if(indeg[ele] == 0) q.add(ele);
+//             }
+//         }
+
+//         if(i == numCourses) return topo;
+//         else return new int[]{};
+//     }
+// }
+
+//using dfs
 class Solution {
+    private boolean dfs(ArrayList<ArrayList<Integer>> adj, Stack<Integer> st, int[] vis, int node){
+        vis[node] =1; //visiting
+        for(int i : adj.get(node)){
+            if(vis[i] ==1) return false;
+            if(vis[i]==0) {
+                if(!dfs(adj,st,vis,i)) return false;
+            }
+        }
+        vis[node] = 2;
+        st.push(node);
+        return true;
+    }
+
     public int[] findOrder(int numCourses, int[][] prerequisites) {
        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
 
@@ -12,35 +73,23 @@ class Solution {
             adj.get(u).add(v);
         }
         
-        int[] indeg = new int[numCourses];
-        //figuring out the indegree of each element 
-        for(int i =0; i< numCourses ;i++){
-            for(int ele : adj.get(i)){
-                indeg[ele]++; //indeg[1]++ (incoming edge to 1)
+        int[] vis = new int[numCourses];
+        Stack<Integer> st = new Stack<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        for(int i = 0; i<numCourses; i++){
+            if(vis[i]==0) {
+                if(!dfs(adj,st,vis,i)) {
+                    return new int[] {};
+                }
             }
         }
         
-        Queue<Integer> q = new LinkedList<Integer>();
-        for(int i =0; i< numCourses ;i++){
-            if(indeg[i]==0) q.add(i); //start from the ele whose indeg is 0
+        int[] res = new int[numCourses];
+        int index = 0;
+        while (!st.isEmpty()) {
+            res[index++] = st.pop();
         }
-            
-        //ArrayList<Integer> topo = new ArrayList<>();
-        int[] topo = new int[numCourses];
-        int i = 0;
-        while(!q.isEmpty()){
-            int node = q.peek(); 
-            q.remove();
-            topo[i++] = node;
-            
-            //since node is in topo sort, remove from indegree
-            for(int ele : adj.get(node)){
-                indeg[ele]--;
-                if(indeg[ele] == 0) q.add(ele);
-            }
-        }
-
-        if(i == numCourses) return topo;
-        else return new int[]{};
+        return res;
     }
 }
