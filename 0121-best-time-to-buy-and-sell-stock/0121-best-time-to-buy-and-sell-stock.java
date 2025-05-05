c14d69e403f3d85c17d922a4d2c2ae08cfcbd1ea
Parnika Roy <79538861+parnika30@users.noreply.github.com>
2025-05-06 00:03:11 +0530

    Time: 2 ms (74.66%), Space: 61.5 MB (82.44%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,13 +1,32 @@
-class Solution {
-    public int maxProfit(int[] prices) {
-        int min = Integer.MAX_VALUE;
-        int max = 0;
-        int n = prices.length;
-        for (int i = 0;i<n;i++){    
-            min = Math.min(min, prices[i]);
-            max = Math.max(max, (prices[i]-min));
-        }
-        return max;
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int min = Integer.MAX_VALUE;
+//         int max = 0;
+//         int n = prices.length;
+//         for (int i = 0;i<n;i++){    
+//             min = Math.min(min, prices[i]);
+//             max = Math.max(max, (prices[i]-min));
+//         }
+//         return max;  
         
+//     }
+// }
+
+
+class Solution { 
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int min = Integer.MAX_VALUE;
+        int max = 0; 
+
+        for(int i =0;i <n ;i ++){
+            if(prices[i]<min){
+                min = prices[i];
+            }else{
+                max = Math.max(max, prices[i] - min);
+            }
+        }
+        return max; 
     }
 }
+
