1079bcbce06e95bd68055de1cdeb0382e43e5fd1
Parnika Roy <79538861+parnika30@users.noreply.github.com>
2025-05-26 21:22:40 +0530

    Time: 7 ms (18.66%), Space: 49.8 MB (39.55%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,26 +1,42 @@
 class Solution {
-    private void bfs(int[][]vis, char[][] grid, int mainRow, int mainCol, int gr, int gc){
-        vis[gr][gc] = 1;
+    private void bfs(int[][]vis, char[][] grid, int r, int c){
+        int gr = grid.length;
+        int gc = grid[0].length;
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] {gr,gc});
+
+        vis[r][c] = 1;
+        q.add(new int[] {r,c});
 
         while(!q.isEmpty()){
             int[] curr = q.poll();
-            int r = curr[0];
-            int c = curr[1]; 
+            int row = curr[0];
+            int col = curr[1]; 
 
-            int[] delR = {-1,1,0,0};
-            int[] delC = {0,0,-1,1};
+            // int[] delR = {-1,1,0,0};
+            // int[] delC = {0,0,-1,1};
 
-            for(int i = 0;i<4;i++){
-                int nr = r + delR[i];
-                int nc = c + delC[i];
+            int[][] directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
 
-                if(nr>=0 && nc>=0 && nr<mainRow && nc<mainCol && vis[nr][nc] == 0 && grid[nr][nc] == '1'){
+            for(int[] dir : directions){
+                int nr = row + dir[0];
+                int nc = col + dir[1];
+
+                if(nr>=0 && nc>=0 && 
+                    nr<gr && nc<gc && vis[nr][nc] == 0 && grid[nr][nc] == '1'){
                     vis[nr][nc] = 1;
                     q.add(new int[] {nr,nc});
                 }
             }
+
+            // for(int i = 0;i<4;i++){
+            //     int nr = r + delR[i];
+            //     int nc = c + delC[i];
+
+            //     if(nr>=0 && nc>=0 && nr<mainRow && nc<mainCol && vis[nr][nc] == 0 && grid[nr][nc] == '1'){
+            //         vis[nr][nc] = 1;
+            //         q.add(new int[] {nr,nc});
+            //     }
+            // }
         }
     }
 
@@ -34,7 +50,8 @@ class Solution {
             for (int j =0; j<m;j++){
                 if(vis[i][j] == 0 && grid[i][j] == '1'){ //grid is land
                     count++;
-                    bfs(vis,grid,n,m,i,j);
+                    bfs(vis,grid,i,j);
+                    bfs(vis,grid,i,j);
                 } 
             }
         }
