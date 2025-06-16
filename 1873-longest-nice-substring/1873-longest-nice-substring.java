09f65f4a97c566364d716faa57c9f515cd54b46a
Parnika Roy <79538861+parnika30@users.noreply.github.com>
2025-06-16 22:02:37 +0530

    Time: 2 ms (80.79%), Space: 42.7 MB (62.2%) - LeetHub

--- 1873-longest-nice-substring/1873-longest-nice-substring.java
+++ 1873-longest-nice-substring/1873-longest-nice-substring.java
@@ -1,32 +1,66 @@
+//O(n^3): 
+// class Solution {
+//     public String longestNiceSubstring(String s) {
+//         String res = "";
+//         for(int i =0; i< s.length(); i++){
+//             for(int j= i+1; j<= s.length(); j++){
+//                 String sub = s.substring(i,j);
+//                 HashSet <Character> upper = new HashSet<>();
+//                 HashSet <Character> lower = new HashSet<>();
+
+//                 for(char c : sub.toCharArray()){
+//                     if(Character.isLowerCase(c)) lower.add(c);
+//                     else upper.add(c);
+//                 }
+
+//                 boolean nice = true;
+//                 for(char c : sub.toCharArray()){
+//                     if(!(upper.contains(Character.toUpperCase(c)) && 
+//                     lower.contains(Character.toLowerCase(c)))) {
+//                         nice = false;
+//                         break;
+//                     }
+//                 }
+
+//                 if(nice && sub.length() > res.length()){
+//                     res = sub;
+//                 }
+//             }
+//         }
+//         return res;
+        
+//     }
+// }
+
+
+// O(N^2)
 class Solution {
     public String longestNiceSubstring(String s) {
-        String res = "";
-        for(int i =0; i< s.length(); i++){
-            for(int j= i+1; j<= s.length(); j++){
-                String sub = s.substring(i,j);
-                HashSet <Character> upper = new HashSet<>();
-                HashSet <Character> lower = new HashSet<>();
+        if (s.length() < 2) return "";
 
-                for(char c : sub.toCharArray()){
-                    if(Character.isLowerCase(c)) lower.add(c);
-                    else upper.add(c);
-                }
-
-                boolean nice = true;
-                for(char c : sub.toCharArray()){
-                    if(!(upper.contains(Character.toUpperCase(c)) && 
-                    lower.contains(Character.toLowerCase(c)))) {
-                        nice = false;
-                        break;
-                    }
-                }
-
-                if(nice && sub.length() > res.length()){
-                    res = sub;
-                }
-            }
+        HashSet <Character> set = new HashSet<>();
+        for(char c : s.toCharArray()){
+            set.add(c);
         }
-        return res;
-        
+
+        for(int i =0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(set.contains(Character.toUpperCase(c)) && 
+                set.contains(Character.toLowerCase(c))){
+                    continue;
+                }
+
+            //DAC 
+            String left = longestNiceSubstring(s.substring(0,i)); 
+            String right = longestNiceSubstring(s.substring(i+1,s.length()));
+
+            if(left.length() >= right.length()) return left;
+            else return right;
+        }
+        return s;   
     }
 }
+
+
+
+
