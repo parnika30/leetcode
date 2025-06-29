8973a6b25c112e1d8a46f10410f7b237c2b42469
Parnika Roy <79538861+parnika30@users.noreply.github.com>
2025-06-29 14:31:30 +0530

    Time: 19 ms (36.06%), Space: 59.7 MB (22.71%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,10 +1,20 @@
 class Solution {
+    // public boolean containsDuplicate(int[] nums) {
+    //     Arrays.sort(nums);
+    //     for(int i=0;i<nums.length-1;i++){
+    //             if(nums[i]==nums[i+1])
+    //             return true;
+    //         }
+    //     return false;
+    //     }
+
+
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++){
-                if(nums[i]==nums[i+1])
-                return true;
-            }
-        return false;
+        HashSet<Integer> set = new HashSet<>();
+        for(int num: nums){
+            set.add(num);
         }
+        if(set.size() == nums.length) return false;
+        else return true;
+    }
 }
