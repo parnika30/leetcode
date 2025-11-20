e4acb8113485166948bad7ee85f31386b8c10601
Parnika Roy <79538861+parnika30@users.noreply.github.com>
2025-11-20 17:20:03 +0530

    Time: 93 ms (37.48%), Space: 48.3 MB (30.41%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -2,21 +2,21 @@ class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
 
         Queue<String> queue = new LinkedList<>();
-        Set<String> words = new HashSet<>(wordList);
-        words.remove(beginWord);
+        Set<String> words = new HashSet<>(wordList); //words : "hot","dot","dog","lot","log"
+        words.remove(beginWord); 
 
-        queue.add(beginWord);
+        queue.add(beginWord); //q : hit
         int level = 0;
 
         while(!queue.isEmpty()){
-            int size = queue.size();
-            level++;
+            int size = queue.size(); //1
+            level++; //1
             for(int i=0;i<size;i++){
-                String currentWord = queue.poll();
+                String currentWord = queue.poll(); //hit
                 if(currentWord.equals(endWord)) return level;
 
-                List<String> neighbours = neighbours(currentWord);
-                for(String s : neighbours){
+                List<String> neighbours = neighbours(currentWord); //hit k neighbours
+                for(String s : neighbours){ //ait
                     if(words.contains(s)){
                         words.remove(s);
                         queue.add(s);
@@ -28,20 +28,20 @@ class Solution {
         
     }
 
-    private List<String> neighbours(String str){
+    private List<String> neighbours(String str){ //hit
         List<String> neighbours = new ArrayList<>();
-        char[] chars = str.toCharArray();
+        char[] chars = str.toCharArray(); //h,i,t
 
         for(int i = 0; i<chars.length; i++){
-            char og = chars[i];
+            char og = chars[i]; //h
 
             for(char c='a'; c<='z'; c++){
                 if(c==og) continue;
-                chars[i] = c; 
-                String newWord = new String(chars);
-                neighbours.add(newWord);
+                chars[i] = c; //a
+                String newWord = new String(chars);//ait
+                neighbours.add(newWord);//ait-zit; not hit
             }
-            chars[i] = og;
+            chars[i] = og;//h
         }
         return neighbours;
     }
