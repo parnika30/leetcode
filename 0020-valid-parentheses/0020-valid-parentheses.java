8a03a0ed9e1b0b766c170b136682b594899922b5
Parnika Roy <79538861+parnika30@users.noreply.github.com>
2026-07-12 14:45:34 +0530

    Time: 3 ms (87.01%), Space: 43.1 MB (85.81%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,26 +2,28 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<Character>();
         for(char c : s.toCharArray()){
+            //checking if the first char is an opening bracket
             if(c == '(' || c == '{' || c == '['){
-                st.push(c);
-            }else {
+                st.push(c); //(
+            }else{
+                //else possibilities are :
+                //1. stack is empty => always invalid. 
                 if(st.isEmpty()){
                     return false;
-                } else {
-                    char popChar = st.pop();
+                }
+                //2. st is not empty and check what's in st by popping
+                else{
+                    char popped = st.pop(); //(
                     if(
-                        ( c == ')' && popChar != '(' )
-                        || 
-                        ( c == ']' && popChar != '[')
-                        || 
-                        ( c == '}' && popChar != '{')
-                        ){
+                        (c==')' && popped != '(') ||
+                        (c==']' && popped != '[') || 
+                        (c=='}' && popped != '{')
+                    ){
                         return false;
                     }
                 }
-
             }
         }
-        return st.isEmpty();
+        return st.isEmpty(); // empty st always true
     }
 }
